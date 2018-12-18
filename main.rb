b6ba995acb335e80621f463a5ce6922d7768cd81
Jason D'Oyley <codemonk_@outlook.com>
2018-12-17 23:48:40 -0500

    Update: Code clean up

--- main.rb
+++ main.rb
@@ -1,42 +1,45 @@
-# Student Management System
-# Author: Jason D. F. D'Oyley
-# Start Date: Mon, Dec 17, 2018
-# Email: codemonk_@outlook.com
-# Version: 0.1
+# =================================== #
+# Student Management System           #
+# Author: Jason D. F. D'Oyley         #
+# Start Date: Mon, Dec 17, 2018       #
+# Email: codemonk_@outlook.com        #
+# Version: 0.1                        #
+# =================================== #
 
-# Key Syntax
+
+# Variable Syntax Key
+# ===========================
 # std - short for student
 
-# Prompt user for student's ID number
+# Capture Student Information
+# ===========================
+# Student identification number
+# First and last name to create full name
+# Student email address
+
 print "Student ID#: "
 std_id = gets.chomp.to_i
 
-# Prompt user for student's first name
 print "First Name: "
 first_name = gets.chomp
 
-# Prompt the user for student's last name
 print "Last Name: "
 last_name = gets.chomp
 
-# Join first and last name to form student's full name
 std_fullname = first_name + " " + last_name
 
-# Prompt user for the student's email
 print "Student Email: "
 std_email = gets.chomp
 
-
-# Clear the system screen before displaying the results back
-# to the user
+# Clear console screen
 system("clear")
 
 
 # Output inputs back to user
 # ==========================
 # Create an app logo at the top of the console
-puts "\t\tStudent Management System(SMS)"
-print "\t\t==============================\n\n"
+puts "\t\tStudent Management System (SMS)"
+print "\t\t===============================\n\n\n"
 
 # Display results
 # ==========================
